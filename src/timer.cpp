a0e34db100e3e7d4a6fc82f0904d32dd2ed91c85
Tomasz Krakowski <tomalla5120x@gmail.com>
2016-12-24 12:46:38 +0100

    Implementacja klasy Timer

---
+++ src/timer.cpp
@@ -0,0 +1,61 @@
+#include "timer.h"
+
+#include <stdexcept>
+#include <signal.h>
+
+Timer::Timer(int signal, int timeout, bool oneshot): m_bOneShot(oneshot)
+{
+	sigevent sev;
+	sev.sigev_notify = SIGEV_SIGNAL;
+	sev.sigev_signo = signal;
+	
+	if(timer_create(CLOCK_REALTIME, &sev, &m_timer) == -1)
+		throw std::runtime_error("Error creating timer");
+	
+	m_nSec = timeout / 1000;
+	m_nNsec = (timeout % 1000) * 1000000;
+}
+
+void Timer::set()
+{
+	itimerspec its;
+	
+	its.it_value.tv_sec = m_nSec;
+	its.it_value.tv_nsec = m_nNsec;
+	
+	if(m_bOneShot)
+	{
+		its.it_interval.tv_sec = 0;
+		its.it_interval.tv_nsec = 0;
+	} else
+	{
+		its.it_interval.tv_sec = m_nSec;
+		its.it_interval.tv_nsec = m_nNsec;
+	}
+	
+	if(timer_settime(m_timer, 0, &its, NULL) == -1)
+		throw std::runtime_error("Error setting timer");
+}
+
+void Timer::unset()
+{
+	itimerspec its;
+	
+	its.it_value.tv_sec = 0;
+	its.it_value.tv_nsec = 0;
+	its.it_interval.tv_sec = 0;
+	its.it_interval.tv_nsec = 0;
+	
+	if(timer_settime(m_timer, 0, &its, NULL) == -1)
+		throw std::runtime_error("Error unsetting timer");
+}
+
+bool Timer::isRunning() const
+{
+	itimerspec its;
+	
+	if(timer_gettime(m_timer, &its) == -1)
+		throw std::runtime_error("Error unsetting timer");
+		
+	return (its.it_value.tv_sec != 0 || its.it_value.tv_nsec != 0);
+}
---
+++ src/timer.h
@@ -0,0 +1,37 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <time.h>
+
+class Timer
+{	
+public:
+	
+	// tworzy obiekt Timera.
+	// * signal - numer sygnału, który ma zostać generowany przy wygasaniu Timera
+	// * timeout - okres Timera (w milisekundach)
+	// * oneshot - czy po wygaśnięciu Timer ma ponownie zacząć odliczać czas
+	// rzuca wyjątek runtime_error, jeżeli wystąpi błąd
+	Timer(int signal, int timeout, bool oneshot);
+	
+	// startuje Timer lub resetuje go, jeżeli już działa
+	// rzuca wyjątek runtime_error, jeżeli wystąpi błąd
+	void set();
+	
+	// zatrzymuje Timer
+	// rzuca wyjątek runtime_error, jeżeli wystąpi błąd
+	void unset();
+	
+	// czy timer jest aktualnie uruchomiony, czy też nie
+	// rzuca wyjątek runtime_error, jeżeli wystąpi błąd
+	bool isRunning() const;
+	
+private:
+	bool m_bOneShot;
+	
+	timer_t m_timer;
+	int m_nSec;
+	int m_nNsec;
+};
+
+#endif
